5705c3253340a7674d2aea690f2d6a076bbc41aa
Olexiy Zamkoviy <olexiy.z@gmail.com>
2013-08-22 23:12:52 +0300

    Fixed bugs

--- filtering-widget.lisp
+++ filtering-widget.lisp
@@ -12,10 +12,10 @@
    (filtering-form-instance :initform nil)))
 
 (defun object->simple-plist (object &rest filters)
-  (loop for i in (sb-mop:class-direct-slots (find-class (class-name  (class-of object)))) append 
-        (let* ((slot (intern (string (sb-mop:slot-definition-name i)) "KEYWORD"))
-               (value (if (slot-boundp object (sb-mop:slot-definition-name i))
-                        (slot-value object (sb-mop:slot-definition-name i))
+  (loop for i in (c2mop:class-direct-slots (find-class (class-name  (class-of object)))) append 
+        (let* ((slot (intern (string (c2mop:slot-definition-name i)) "KEYWORD"))
+               (value (if (slot-boundp object (c2mop:slot-definition-name i))
+                        (slot-value object (c2mop:slot-definition-name i))
                         "Unbound")))
           (list slot (if (getf filters slot) (funcall (getf filters slot) value) value)))))
 
--- weblocks-filtering-widget.asd
+++ weblocks-filtering-widget.asd
@@ -9,7 +9,7 @@
     (push :clsql dependencies))
    (eval `(defsystem weblocks-filtering-widget
            :name "Weblocks filtering widget"
-           :version "0.3.5"
+           :version "0.3.6"
            :author "Olexiy Zamkoviy"
            :licence "LLGPL"
            :description "Filtering widget for weblocks framework"
